978775d41b7a2e52cc4dbf786b03998d8af6fa57
ani8897 <aniket8897@gmail.com>
2018-12-26 12:33:07 +0530

    Changes made to sensor scripts

--- sensor/config1-1sensor.sh
+++ sensor/config1-1sensor.sh
@@ -3,6 +3,6 @@ do
     if [[ $(($num % 2)) -eq 1 ]]
     then
         i=$(printf "%02d" $num)
-        python3 aqi_sensor.py --sensor-id $num --source ../../../TrainingData/0010${i}_Data.csv -k $1 &
+        python3 aqi_sensor.py --sensor-id $num --source ../../../TrainingData/0010${i}_Data.csv -k $1 -d 0.1 &
     fi
 done
--- sensor/three-child.sh
+++ sensor/three-child.sh
@@ -1,3 +1,3 @@
-python3 aqi_sensor.py --sensor_id 1 --source ../data/Cluster7_Data.csv -k $1 &
-python3 aqi_sensor.py --sensor_id 2 --source ../data/Cluster6_Data.csv -k $1 &
-python3 aqi_sensor.py --sensor_id 3 --source ../data/Cluster5_Data.csv -k $1 &
+python3 aqi_sensor.py --sensor-id 1 --source ../data/001020_Data.csv -d $1 -k $2 &
+python3 aqi_sensor.py --sensor-id 2 --source ../data/001024_Data.csv -d $1 -k $2 &
+python3 aqi_sensor.py --sensor-id 3 --source ../data/001029_Data.csv -d $1 -k $2 &
