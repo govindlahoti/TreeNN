c6a189e8ac2353688322dd9c83a8498126615fc8
aniket <aniket8897@gmail.com>
2019-01-06 11:26:55 +0530

    Four-child config added

---
+++ sensor/four-child.sh
@@ -0,0 +1,4 @@
+python3 aqi_sensor.py --sensor-id 1 --source ../data/001021_Data.csv -d $1 -k $2 &
+python3 aqi_sensor.py --sensor-id 2 --source ../data/001029_Data.csv -d $1 -k $2 &
+python3 aqi_sensor.py --sensor-id 3 --source ../data/001027_Data.csv -d $1 -k $2 &
+python3 aqi_sensor.py --sensor-id 4 --source ../data/001026_Data.csv -d $1 -k $2 &
