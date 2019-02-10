89c0f16d2c57436e33fbc85f56f9377dc096352f
ani8897 <aniket8897@gmail.com>
2019-02-10 20:21:37 +0530

    Config modified for test expbox

--- sensor/test.sh
+++ sensor/test.sh
@@ -1,1 +1,2 @@
-python3 aqi_sensor.py --sensor-id 1 --source ../data/001024_Data.csv -d $1 -k $2 & 
+python3 aqi_sensor.py --sensor-id 1 --source ~/Simulator/original_data/Cluster1_Data.csv -d $1 -k $2 &
+python3 aqi_sensor.py --sensor-id 2 --source ~/Simulator/original_data/Cluster7_Data.csv -d $1 -k $2 & 
