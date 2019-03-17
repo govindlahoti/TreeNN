9f87fe2ee12d6e6d2af3c5975262dc82cf95e470
ani8897 <aniket8897@gmail.com>
2019-03-17 23:23:27 +0530

    Config files and sensor scripts added

---
+++ sensor/mnist/five-child.sh
@@ -0,0 +1,10 @@
+python3 mnist_sensor.py --sensor-id 20 --source ~/Simulator/mnist_data/train/biased-0.csv -d $1 -k $2 & 
+python3 mnist_sensor.py --sensor-id 21 --source ~/Simulator/mnist_data/train/biased-1.csv -d $1 -k $2 & 
+python3 mnist_sensor.py --sensor-id 22 --source ~/Simulator/mnist_data/train/biased-2.csv -d $1 -k $2 & 
+python3 mnist_sensor.py --sensor-id 23 --source ~/Simulator/mnist_data/train/biased-3.csv -d $1 -k $2 & 
+python3 mnist_sensor.py --sensor-id 24 --source ~/Simulator/mnist_data/train/biased-4.csv -d $1 -k $2 & 
+python3 mnist_sensor.py --sensor-id 20 --source ~/Simulator/mnist_data/train/biased-5.csv -d $1 -k $2 & 
+python3 mnist_sensor.py --sensor-id 21 --source ~/Simulator/mnist_data/train/biased-6.csv -d $1 -k $2 & 
+python3 mnist_sensor.py --sensor-id 22 --source ~/Simulator/mnist_data/train/biased-7.csv -d $1 -k $2 & 
+python3 mnist_sensor.py --sensor-id 23 --source ~/Simulator/mnist_data/train/biased-8.csv -d $1 -k $2 & 
+python3 mnist_sensor.py --sensor-id 24 --source ~/Simulator/mnist_data/train/biased-9.csv -d $1 -k $2 & 
